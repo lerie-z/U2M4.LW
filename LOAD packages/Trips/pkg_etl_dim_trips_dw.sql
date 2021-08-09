587d529b4b80bce8587918bf2b212742be83e07c
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-08-09 04:05:01 +0300

    Loaded from Storage Level

---
+++ LOAD packages/Trips/pkg_etl_dim_trips_dw.sql
@@ -0,0 +1,9 @@
+--==============================================================
+-- PACKAGE dw_cls_lvl.pkg_etl_dim_trips_dw:        SPECIFICATION
+--==============================================================
+
+CREATE OR REPLACE PACKAGE dw_level.pkg_etl_dim_trips_dw
+AS
+   PROCEDURE load_trips;
+END;
+/
