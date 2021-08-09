2456a76862b4eedabbe6857c9ddc68e927baaf83
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-08-09 04:06:18 +0300

    Loaded from Storage Level

---
+++ LOAD packages/Vehicles/pkg_etl_dim_vehicles_dw.sql
@@ -0,0 +1,9 @@
+--==============================================================
+-- PACKAGE dw_cls_lvl.pkg_etl_dim_vehicles_dw:     SPECIFICATION
+--==============================================================
+
+CREATE OR REPLACE PACKAGE dw_level.pkg_etl_dim_vehicles_dw
+AS
+   PROCEDURE load_vehicles;
+END;
+/
