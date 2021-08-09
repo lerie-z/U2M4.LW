f9f63f614facdc48c0ecd003d90882001a9c7c5b
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-08-09 04:01:34 +0300

    loaded from Storage Level

---
+++ LOAD packages/Clients/pkg_etl_dim_clients_dw.sql
@@ -0,0 +1,9 @@
+--==============================================================
+-- PACKAGE dw_cls_lvl.pkg_etl_dim_clients_dw:      SPECIFICATION
+--==============================================================
+
+CREATE OR REPLACE PACKAGE dw_cls_lvl.pkg_etl_dim_clients_dw
+AS
+   PROCEDURE load_clients;
+END;
+/
