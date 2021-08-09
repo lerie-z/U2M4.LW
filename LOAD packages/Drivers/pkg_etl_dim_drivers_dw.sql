f7f180148319667ac64c3425d7cbe4819e143e8a
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-08-09 04:03:13 +0300

    Add files via upload

---
+++ LOAD packages/Drivers/pkg_etl_dim_drivers_dw.sql
@@ -0,0 +1,9 @@
+--==============================================================
+-- PACKAGE dw_cls_lvl.pkg_etl_dim_drivers_dw:      SPECIFICATION
+--==============================================================
+
+CREATE OR REPLACE PACKAGE dw_level.pkg_etl_dim_drivers_dw
+AS
+   PROCEDURE load_drivers;
+END;
+/
