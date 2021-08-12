92abbb9347a5f03ad2dbf7b622b6cb3277e0b4a6
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-08-12 18:43:47 +0300

    Delete pkg_etl_dim_vehicles_dw.sql

--- LOAD packages/Vehicles/pkg_etl_dim_vehicles_dw.sql
+++
@@ -1,9 +0,0 @@
---==============================================================
--- PACKAGE dw_cls_lvl.pkg_etl_dim_vehicles_dw:     SPECIFICATION
---==============================================================
-
-CREATE OR REPLACE PACKAGE dw_level.pkg_etl_dim_vehicles_dw
-AS
-   PROCEDURE load_vehicles;
-END;
-/
