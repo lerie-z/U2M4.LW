b4277204f0a261dc5b00dc4bb01facd050eaca19
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-08-12 18:38:18 +0300

    Delete pkg_etl_dim_clients_dw.sql

--- LOAD packages/Clients/pkg_etl_dim_clients_dw.sql
+++
@@ -1,9 +0,0 @@
---==============================================================
--- PACKAGE dw_cls_lvl.pkg_etl_dim_clients_dw:      SPECIFICATION
---==============================================================
-
-CREATE OR REPLACE PACKAGE dw_cls_lvl.pkg_etl_dim_clients_dw
-AS
-   PROCEDURE load_clients;
-END;
-/
