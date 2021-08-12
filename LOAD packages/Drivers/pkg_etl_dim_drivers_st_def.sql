1e4a0675d4a841b650966da062fc4bca0dc7b79e
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-08-12 18:40:21 +0300

    refactored code

---
+++ LOAD packages/Drivers/pkg_etl_dim_drivers_st_def.sql
@@ -0,0 +1,9 @@
+--==============================================================
+-- PACKAGE st_layer.pkg_etl_dim_drivers_st:        SPECIFICATION
+--==============================================================
+
+CREATE OR REPLACE PACKAGE st_layer.pkg_etl_dim_drivers_st
+AS
+   PROCEDURE load_drivers;
+END;
+/
