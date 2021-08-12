679397954ff1d61c48d9956e41ec16fb25933ee1
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-08-12 18:42:56 +0300

    Delete pkg_etl_dim_trips_dw.sql

--- LOAD packages/Trips/pkg_etl_dim_trips_dw.sql
+++
@@ -1,9 +0,0 @@
---==============================================================
--- PACKAGE dw_cls_lvl.pkg_etl_dim_trips_dw:        SPECIFICATION
---==============================================================
-
-CREATE OR REPLACE PACKAGE dw_level.pkg_etl_dim_trips_dw
-AS
-   PROCEDURE load_trips;
-END;
-/
