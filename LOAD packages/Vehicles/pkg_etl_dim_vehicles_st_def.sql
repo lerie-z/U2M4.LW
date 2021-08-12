14ec35c6aa5cc848892c474d18ccdff5ddb2b655
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-08-12 18:43:35 +0300

    refactored code

---
+++ LOAD packages/Vehicles/pkg_etl_dim_vehicles_st_def.sql
@@ -0,0 +1,9 @@
+--==============================================================
+-- PACKAGE st_layer.pkg_etl_dim_vehicles_st:       SPECIFICATION
+--==============================================================
+
+CREATE OR REPLACE PACKAGE st_layer.pkg_etl_dim_vehicles_st
+AS
+   PROCEDURE load_vehicles;
+END;
+/
