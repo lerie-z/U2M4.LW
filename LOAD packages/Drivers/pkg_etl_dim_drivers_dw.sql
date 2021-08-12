d393691535e47e5c4fcb1e6a19bc6acc01ff96f8
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-08-12 18:40:43 +0300

    Delete pkg_etl_dim_drivers_dw.sql

--- LOAD packages/Drivers/pkg_etl_dim_drivers_dw.sql
+++
@@ -1,9 +0,0 @@
---==============================================================
--- PACKAGE dw_cls_lvl.pkg_etl_dim_drivers_dw:      SPECIFICATION
---==============================================================
-
-CREATE OR REPLACE PACKAGE dw_level.pkg_etl_dim_drivers_dw
-AS
-   PROCEDURE load_drivers;
-END;
-/
